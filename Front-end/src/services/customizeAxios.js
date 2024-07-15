7a6a0cafb07144a45344e273b65a7bf281f019fa
th3y3m <truongtanhuy3006@gmail.com>
2024-07-16 01:40:42 +0700

    Update customizeAxios.js

--- Front-end/src/services/customizeAxios.js
+++ Front-end/src/services/customizeAxios.js
@@ -7,11 +7,18 @@ const instance = axios.create({
 instance.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    return response.data ? response.data : {statuscode: response.status};
+    return response.data ? response.data : { statuscode: response.status };
 }, function (error) {
-    console.log(">>>>>>>>> Check error: " + error.name);
-    console.log(">>>>>>>>> Check error: " + error.response);
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
+    let res = {};
+    if (error.response) {
+        res.data = error.response.data;
+        res.status = error.response.status;
+        res.headers = error.response.headers;
+
+    } else if (error.request) {
+        console.log(error.request);
+    } else {
+        console.log('Error', error.message);
+    }
+    return res;
 });

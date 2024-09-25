5a734f3682eee8f9bb957222560c0d21a95a43b4
th3y3m <truongtanhuy3006@gmail.com>
2024-09-25 09:23:57 +0700

    update component responsive

--- Front-end/src/components/News/News.js
+++ Front-end/src/components/News/News.js
@@ -3,12 +3,19 @@ import { useNavigate } from "react-router-dom";
 const News = ({ news }) => {
     const navigate = useNavigate();
     return (
-        <div key={news.id} className="flex items-center space-x-4 bg-white shadow-2xl rounded-lg p-4 h-full"
-            onClick={() => navigate(`/news-details/${news.newId}`)}> {/* Adjusted height */}
-            <img src={news.image} alt={news.title} className="h-40 w-auto object-cover rounded-lg" /> {/* Adjusted size */}
-            <div className="flex flex-col justify-center">
-                <h3 className="text-lg font-semibold">{news.title}</h3>
-                <p className="text-gray-600 text-sm">{news.description}</p>
+        <div
+            key={news.id}
+            className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 bg-white shadow-2xl rounded-lg p-4 h-full"
+            onClick={() => navigate(`/news-details/${news.newId}`)}
+        >
+            <img
+                src={news.image}
+                alt={news.title}
+                className="h-40 w-full md:w-auto object-cover rounded-lg"
+            />
+            <div className="flex flex-col justify-center text-center md:text-left">
+                <h3 className="text-lg md:text-xl font-semibold">{news.title}</h3>
+                <p className="text-gray-600 text-sm md:text-base">{news.description}</p>
             </div>
         </div>
     );

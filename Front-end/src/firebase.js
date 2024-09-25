318128ddf6f7330d75779f616ae14dd60a5b089c
th3y3m <truongtanhuy3006@gmail.com>
2024-09-25 09:34:50 +0700

    update .env

--- Front-end/src/firebase.js
+++ Front-end/src/firebase.js
@@ -10,14 +10,16 @@ import 'firebase/compat/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDAPz7GMnppP018Kpm20stKsAdbHOcJHc4",
-  authDomain: "court-callers.firebaseapp.com",
-  projectId: "court-callers",
-  storageBucket: "court-callers.appspot.com",
-  messagingSenderId: "48866245430",
-  appId: "1:48866245430:web:a9eb7ef7e76077765c5197",
-  measurementId: "G-KEMR387K1Q"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
+export default firebaseConfig;
 
 // Use this to initialize the firebase App
 const firebaseApp = firebase.initializeApp(firebaseConfig);
@@ -27,4 +29,4 @@ const auth = firebase.auth();
 
 const storageDb = getStorage(firebaseApp)
 
-export {firebaseApp, auth, storageDb}
+export { firebaseApp, auth, storageDb }
